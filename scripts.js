2e7fb03cdcd97284115f3c725abc870e25855e4f
AqueousOtter <dustin. brown.k@gmail.com>
2021-06-05 12:41:51 -0400

    Added classes for buttons. TODO: Fix logic for button clicks.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,47 @@
+const addition = document.getElementById("add");
+const subtraction = document.getElementById("subtract");
+const multiplication = document.getElementById("multiply");
+const division = document.getElementById("divide");
+const squareRoot = document.getElementById("sqrt");
+const percentage = document.getElementById("percentage");
+const buttons = document.querySelectorAll("button");
+const expression = document.getElementById("expression");
+
+
+
+let operator;
+let num1;
+let num2;
+let operatorsClicked = 0;
+let equation;
+//adds listeners to all buttons
+//TODO: work out logic to only solve two numbers at a time and use answer if another operator is pressed
+for (const btn of buttons){
+    btn.addEventListener("click", function() {
+        expression.innerHTML += (this.innerHTML); //displays buttons pressed
+
+        if (btn.classList == 'operator'){
+            operatorsClicked += 1;
+            if(operatorsClicked > 1){
+                let problem = expression.innerHTML.split(" ");
+                console.log(problem);
+
+            }
+/*             else {
+                expression.innerHTML += (this.innerHTML); //displays buttons pressed
+            } */
+        }
+/*         else{
+            expression.innerHTML += (this.innerHTML); //displays buttons pressed
+
+        } */
+    });
+}
+
+
+
+
+
 function add(num1, num2){
     return (num1 + num2);
 };
